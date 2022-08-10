7790f158a6741904d4a8779a8c87a5a515e91a82
fumiama <41315874+fumiama@users.noreply.github.com>
2022-08-10 22:18:23 +0800

    adapt macos cpu

--- train.py
+++ train.py
@@ -71,7 +71,10 @@ def prepare_directories_and_logger(output_directory, log_directory, rank):
 
 
 def load_model(hparams):
-    model = Tacotron2(hparams).cuda()
+    model = Tacotron2(hparams)
+    if hparams.enable_cuda:
+        model = model.cuda()
+
     if hparams.fp16_run:
         model.decoder.attention_layer.score_mask_value = finfo('float16').min
 
